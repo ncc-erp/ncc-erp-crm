2c90a7bb836d3ea81b3bef7bdcfb1df666b37be6
VietHoangNCC <hoang.nguyenviet1@ncc.asia>
2023-09-08 08:43:56 +0700

    Fix bug 41027 - Filter date

--- aspnet-core/src/CRM.Core/Uitls/CommonUtils.cs
+++ aspnet-core/src/CRM.Core/Uitls/CommonUtils.cs
@@ -5,7 +5,7 @@ namespace CRM.Uitls
 {
     public static class CommonUtils
     {
-        public const string
-        ExistedInfor = "Email or phone had already existed!!!";
+        public const string ExistedInfor = "Email or phone had already existed!!!";
+        public const string ValidationDateRange = "Start Date must be less than End Date!";
     }
 }
